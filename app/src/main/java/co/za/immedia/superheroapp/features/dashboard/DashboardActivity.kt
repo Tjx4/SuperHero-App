8160f438a05266ab0d70daa81be4c537164e5994
Tshepo <rocboyt@gmail.com>
2020-12-03 08:47:57 +0200

    added dashboard

--- app/src/main/java/co/za/immedia/superheroapp/features/dashboard/DashboardActivity.kt
+++ app/src/main/java/co/za/immedia/superheroapp/features/dashboard/DashboardActivity.kt
@@ -1,17 +1,35 @@
 package co.za.immedia.superheroapp.features.dashboard
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import co.za.immedia.superheroapp.R
+import co.za.immedia.superheroapp.databinding.ActivityDashboardBinding
+import co.za.immedia.superheroapp.features.base.activities.BaseActivity
 
-class DashboardActivity : AppCompatActivity() {
+class DashboardActivity : BaseActivity() {
     // https://superheroapi.com/api/3899774550042021
+
+    private lateinit var binding: ActivityDashboardBinding
+    lateinit var dashboardViewModel: DashboardViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        var application = requireNotNull(this).application
+        var viewModelFactory = DashboardViewModelFactory(application)
+
+        dashboardViewModel = ViewModelProviders.of(this, viewModelFactory).get(DashboardViewModel::class.java)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
+        binding.dashboardViewModel = dashboardViewModel
+        binding.lifecycleOwner = this
+
+        addObservers()
 
         supportActionBar?.elevation = 0f
         supportActionBar?.title = "Heros"
+    }
+
+    private fun addObservers() {
+        // dashboardViewModel.showLoading.observe(this, Observer { toggleShow(it) })
     }
 }
--- app/src/main/java/co/za/immedia/superheroapp/features/dashboard/DashboardViewModel.kt
+++ app/src/main/java/co/za/immedia/superheroapp/features/dashboard/DashboardViewModel.kt
@@ -1,10 +1,18 @@
 package co.za.immedia.superheroapp.features.dashboard
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
 import co.za.immedia.superheroapp.features.base.viewmodels.BaseVieModel
+import co.za.immedia.superheroapp.models.SuperHero
 
 class DashboardViewModel(application: Application, val dashboardRepository: DashboardRepository) : BaseVieModel(application) {
 
+    private var _superHero: MutableLiveData<SuperHero> = MutableLiveData()
+    val superHero: MutableLiveData<SuperHero>
+        get() = _superHero
 
+    init {
+        _superHero.value = SuperHero("Iron man")
+    }
 
 }
