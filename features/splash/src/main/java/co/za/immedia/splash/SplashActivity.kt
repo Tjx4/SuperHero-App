070b09385ca9e653bdd18b759d2863c57f3d9b98
Tshepo <rocboyt@gmail.com>
2020-12-10 14:08:37 +0200

    Added splash module

---
+++ features/splash/src/main/java/co/za/immedia/splash/SplashActivity.kt
@@ -0,0 +1,11 @@
+package co.za.immedia.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
