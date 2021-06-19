cd8759391e3fafa010308ee757b222f24ea04b34
Tshepo <rocboyt@gmail.com>
2021-06-19 19:58:21 +0200

    refactored code

--- heroRating/src/main/java/co/za/immedia/herorating/ViewModelFactory.kt
+++ heroRating/src/main/java/co/za/immedia/herorating/ViewModelFactory.kt
@@ -1,4 +1,22 @@
 package co.za.immedia.herorating
 
-class ViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import co.za.immedia.networking.API
+import co.za.immedia.persistence.room.SuperheroDB
+import co.za.immedia.repositories.SuperheroesRepository
+import java.lang.IllegalArgumentException
+
+class ViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        when {
+            modelClass.isAssignableFrom(RatingViewModel::class.java) -> {
+                val dbRepository = SuperheroesRepository(API.retrofit, SuperheroDB.getInstance(application))
+                return RatingViewModel(application, dbRepository) as T
+            }
+        }
+
+        throw IllegalArgumentException("Unknown ViewModel Class")
+    }
 }
