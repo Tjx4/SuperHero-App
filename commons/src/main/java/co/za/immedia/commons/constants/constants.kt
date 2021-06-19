e57254a7993e5577abf3aa4f590e086789977a4c
Tshepo <rocboyt@gmail.com>
2021-06-19 20:07:27 +0200

    refactored code

--- commons/src/main/java/co/za/immedia/commons/constants/constants.kt
+++ commons/src/main/java/co/za/immedia/commons/constants/constants.kt
@@ -1,13 +1,11 @@
 package co.za.immedia.commons.constants
 
-
 const val PAYLOAD_KEY = "payload"
 const val ACTIVITY_TRANSITION = "activity_transition"
 const val SUPERHERO = "superhero"
 const val FAV = "favourite"
 const val LAYOUT = "layout"
 const val TITLE = "tittle"
-
 const val INTRO_ACTIVITY = "co.za.immedia.intro.IntroActivity"
 const val SEARCH_ACTIVITY = "co.za.immedia.search.SearchActivity"
 const val VIEW_SUPRERHERO_ACTIVITY = "co.za.immedia.superhero.ViewSuperheroActivity"
