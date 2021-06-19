095733dfb3c727230babd082ddc683d24ad229e6
Tshepo <rocboyt@gmail.com>
2021-06-19 18:49:10 +0200

    pre vm

--- commons/src/main/java/co/za/immedia/commons/base/viewmodels/BaseVieModel.kt
+++ commons/src/main/java/co/za/immedia/commons/base/viewmodels/BaseVieModel.kt
@@ -2,9 +2,6 @@ package co.za.immedia.commons.base.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 
 abstract class BaseVieModel(val app: Application) : AndroidViewModel(app){
 }
