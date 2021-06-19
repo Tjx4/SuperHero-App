41108b8b482a0d222311cdc0f6e30abd5be7dbe0
Tshepo <rocboyt@gmail.com>
2021-06-19 20:12:36 +0200

    refactored code

--- networking/src/main/java/co/za/immedia/networking/RetrofitHelper.kt
+++ networking/src/main/java/co/za/immedia/networking/RetrofitHelper.kt
@@ -1,9 +1,7 @@
 package co.za.immedia.networking
 
 import co.za.immedia.commons.models.Appearance
-import co.za.immedia.commons.models.Connections
 import co.za.immedia.commons.models.SearchResult
-import co.za.immedia.commons.models.Work
 import retrofit2.http.*
 
 interface RetrofitHelper {
